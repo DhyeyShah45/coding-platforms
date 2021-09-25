28bba566d20eed79f26449a9845b154ec8ea471c
DhyeyShah45 <80476304+DhyeyShah45@users.noreply.github.com>
2021-09-25 15:27:41 +0530

    Create DayTwentyOne.java

---
+++ Java/HackerRank-30DOC/DayTwentyOne.java
@@ -0,0 +1,47 @@
+/**
+* @Autor - Dhyey Shah
+* @problem - https://www.hackerrank.com/challenges/30-generics/problem
+*/
+import java.util.*;
+
+class Printer <T> {
+
+    static <T> void printArray(T array[]){
+        for (T a : array)
+            System.out.println(a);
+    }
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+
+}
+
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
